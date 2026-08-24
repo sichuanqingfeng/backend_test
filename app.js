403706c87a05ad15d71b53786a31f2d320d3b1a2
viki <527789254@qq.com>
2026-08-24 23:14:53 +0800

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from ACK Node.js!\n');
+  res.end('Hello from ACK Node.js!   ' + Math.floor(Math.random() * 10000));
 });
 
 server.listen(port, () => {
